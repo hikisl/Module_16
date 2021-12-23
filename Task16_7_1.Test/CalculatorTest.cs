79f5db6c1a0668b3461deeaaa93becdeb77416ed
oldboyee <fuukingy@gmail.com>
2021-12-23 18:50:21 +0200

    Добавьте файлы проекта.

---
+++ Task16_7_1.Test/CalculatorTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task_16_7_1;
+
+namespace Task16_7_1.Test
+{
+        [TestFixture]
+    class CalculatorTest
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(200, 10) == 210);
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(200, 10) == 190);
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(20, 10) == 200);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(200, 10) == 20);
+        }
+
+        [Test]
+        public void Division_MustThrowException()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(20, 0));
+        }
+    }
+}
---
+++ Task_16_7_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_16_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
